0d176f6e46302624b97660594696289d401e6ee3
Shakeel Ahmad <shakeel2717@gmail.com>
2023-09-27 23:16:58 +0500

    Auth Complete, user Dashboard Created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,110 +1,254 @@
-
 <!doctype html>
 <html lang="en">
 
-    <head>
-        
-        <meta charset="utf-8" />
-        <title>Register | Nazox - Responsive Bootstrap 4 Admin Dashboard</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesdesign" name="author" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+<head>
 
-        <!-- Bootstrap Css -->
-        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8" />
+    <title>Dashboard | Nazox - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/assets/images/favicon.ico">
 
-    </head>
+    <!-- jquery.vectormap css -->
+    <link href="/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet"
+        type="text/css" />
 
-    <body class="auth-body-bg">
-        <div class="home-btn d-none d-sm-block">
-            <a href="index.html"><i class="mdi mdi-home-variant h2 text-white"></i></a>
-        </div>
-        <div>
-            <div class="container-fluid p-0">
-                <div class="row no-gutters">
-                    <div class="col-lg-4">
-                        <div class="authentication-page-content p-4 d-flex align-items-center min-vh-100">
-                            <div class="w-100">
-                                <div class="row justify-content-center">
-                                    <div class="col-lg-9">
-                                        <div>
-                                            <div class="text-center">
-                                                <div>
-                                                    <a href="index.html" class="logo"><img src="assets/images/logo-dark.png" height="20" alt="logo"></a>
-                                                </div>
-    
-                                                <h4 class="font-size-18 mt-4">Register account</h4>
-                                                <p class="text-muted">Get your free Nazox account now.</p>
-                                            </div>
+    <!-- DataTables -->
+    <link href="/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
 
-                                            <div class="p-2 mt-5">
-                                                <form class="form-horizontal" action="index.html">
+    <!-- Responsive datatable examples -->
+    <link href="/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
 
-                                                    <div class="form-group auth-form-group-custom mb-4">
-                                                        <i class="ri-mail-line auti-custom-input-icon"></i>
-                                                        <label for="useremail">Email</label>
-                                                        <input type="email" class="form-control" id="useremail" placeholder="Enter email">
-                                                    </div>
-                    
-                                                    <div class="form-group auth-form-group-custom mb-4">
-                                                        <i class="ri-user-2-line auti-custom-input-icon"></i>
-                                                        <label for="username">Username</label>
-                                                        <input type="text" class="form-control" id="username" placeholder="Enter username">
-                                                    </div>
-                            
-                                                    <div class="form-group auth-form-group-custom mb-4">
-                                                        <i class="ri-lock-2-line auti-custom-input-icon"></i>
-                                                        <label for="userpassword">Password</label>
-                                                        <input type="password" class="form-control" id="userpassword" placeholder="Enter password">
-                                                    </div>
-                            
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
 
-                                                    <div class="text-center">
-                                                        <button class="btn btn-primary w-md waves-effect waves-light" type="submit">Register</button>
-                                                    </div>
+</head>
 
-                                                    <div class="mt-4 text-center">
-                                                        <p class="mb-0">By registering you agree to the Nazox <a href="#" class="text-primary">Terms of Use</a></p>
-                                                    </div>
-                                                </form>
-                                            </div>
+<body data-sidebar="dark">
 
-                                            <div class="mt-5 text-center">
-                                                <p>Already have an account ? <a href="auth-login.html" class="font-weight-medium text-primary"> Login</a> </p>
-                                                <p>© 2020 Nazox. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign</p>
-                                            </div>
-                                        </div>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
 
+        <header id="page-topbar">
+            <div class="navbar-header">
+                <div class="d-flex">
+                    <!-- LOGO -->
+                    <div class="navbar-brand-box">
+                        <a href="index.html" class="logo logo-dark">
+                            <span class="logo-sm">
+                                <img src="/assets/images/logo-sm-dark.png" alt="" height="22">
+                            </span>
+                            <span class="logo-lg">
+                                <img src="/assets/images/logo-dark.png" alt="" height="20">
+                            </span>
+                        </a>
+
+                        <a href="index.html" class="logo logo-light">
+                            <span class="logo-sm">
+                                <img src="/assets/images/logo-sm-light.png" alt="" height="22">
+                            </span>
+                            <span class="logo-lg">
+                                <img src="/assets/images/logo-light.png" alt="" height="20">
+                            </span>
+                        </a>
+                    </div>
+
+                    <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect"
+                        id="vertical-menu-btn">
+                        <i class="ri-menu-2-line align-middle"></i>
+                    </button>
+                </div>
+
+                <div class="d-flex">
+                    <div class="dropdown d-none d-lg-inline-block ml-1">
+                        <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            <i class="ri-apps-2-line"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                            <div class="px-lg-2">
+                                <div class="row no-gutters">
+                                    <div class="col">
+                                        <a class="dropdown-icon-item" href="#">
+                                            <img src="/assets/images/brands/github.png" alt="Github">
+                                            <span>Dashboard</span>
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a class="dropdown-icon-item" href="#">
+                                            <img src="/assets/images/brands/bitbucket.png" alt="bitbucket">
+                                            <span>Deposit</span>
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a class="dropdown-icon-item" href="#">
+                                            <img src="/assets/images/brands/dribbble.png" alt="dribbble">
+                                            <span>Trade</span>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-8">
-                        <div class="authentication-bg">
-                            <div class="bg-overlay"></div>
+
+                    <div class="dropdown d-none d-lg-inline-block ml-1">
+                        <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
+                            <i class="ri-fullscreen-line"></i>
+                        </button>
+                    </div>
+
+                    <div class="dropdown d-inline-block">
+                        <button type="button" class="btn header-item noti-icon waves-effect"
+                            id="page-header-notifications-dropdown" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                            <i class="ri-notification-3-line"></i>
+                            <span class="noti-dot"></span>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"
+                            aria-labelledby="page-header-notifications-dropdown">
+                            <div class="p-3">
+                                <div class="row align-items-center">
+                                    <div class="col">
+                                        <h6 class="m-0"> Notifications </h6>
+                                    </div>
+                                    <div class="col-auto">
+                                        <a href="#!" class="small"> View All</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div data-simplebar style="max-height: 230px;">
+                            </div>
+                            <div class="p-2 border-top">
+                                <a class="btn btn-sm btn-link font-size-14 btn-block text-center"
+                                    href="javascript:void(0)">
+                                    <i class="mdi mdi-arrow-right-circle mr-1"></i> View More..
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="dropdown d-inline-block user-dropdown">
+                        <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="rounded-circle header-profile-user" src="/assets/images/users/avatar-2.jpg"
+                                alt="Header Avatar">
+                            <span class="d-none d-xl-inline-block ml-1">{{ auth()->user()->name }}</span>
+                            <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <!-- item-->
+                            <a class="dropdown-item" href="#"><i class="ri-user-line align-middle mr-1"></i>
+                                Profile</a>
+                            <a class="dropdown-item" href="#"><i
+                                    class="ri-wallet-2-line align-middle mr-1"></i> My Wallet</a>
+                            <a class="dropdown-item d-block" href="#"><span
+                                    class="badge badge-success float-right mt-1">11</span><i
+                                    class="ri-settings-2-line align-middle mr-1"></i> Settings</a>
+                            <a class="dropdown-item" href="#"><i
+                                    class="ri-lock-unlock-line align-middle mr-1"></i> Lock screen</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item text-danger" href="#"><i
+                                    class="ri-shut-down-line align-middle mr-1 text-danger"></i> Logout</a>
                         </div>
                     </div>
                 </div>
             </div>
+        </header>
+        <div class="vertical-menu">
+            <div data-simplebar class="h-100">
+                <div id="sidebar-menu">
+                    <ul class="metismenu list-unstyled" id="side-menu">
+                        <li class="menu-title">Menu</li>
+
+                        <li>
+                            <a href="{{ route('user.dashboard.index') }}" class="waves-effect">
+                                <i class="ri-dashboard-line"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        <div class="main-content">
 
-        
+            <div class="page-content">
+                <div class="container-fluid">
 
-        <!-- JAVASCRIPT -->
-        <script src="assets/libs/jquery/jquery.min.js"></script>
-        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
-        <script src="assets/libs/simplebar/simplebar.min.js"></script>
-        <script src="assets/libs/node-waves/waves.min.js"></script>
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-flex align-items-center justify-content-between">
+                                <h4 class="mb-0">@yield('title')</h4>
 
-        <script src="assets/js/app.js"></script>
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a
+                                                href="{{ route('user.dashboard.index') }}">{{ env('APP_NAME') }}</a>
+                                        </li>
+                                        <li class="breadcrumb-item active">@yield('title')</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @yield('content')
+                </div>
+            </div>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> © {{ env('APP_NAME') }}. All Rights Reserved
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="text-sm-right d-none d-sm-block">
+                                Crafted with <i class="mdi mdi-heart text-danger"></i> by {{ env('APP_NAME') }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <!-- end main content-->
 
-    </body>
+    </div>
+
+    <!-- JAVASCRIPT -->
+    <script src="/assets/libs/jquery/jquery.min.js"></script>
+    <script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/libs/metismenu/metisMenu.min.js"></script>
+    <script src="/assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="/assets/libs/node-waves/waves.min.js"></script>
+
+    <!-- apexcharts -->
+    <script src="/assets/libs/apexcharts/apexcharts.min.js"></script>
+
+    <!-- jquery.vectormap map -->
+    <script src="/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js"></script>
+
+    <!-- Required datatable js -->
+    <script src="/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Responsive examples -->
+    <script src="/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+    <script src="/assets/js/pages/dashboard.init.js"></script>
+
+    <script src="/assets/js/app.js"></script>
+
+</body>
+
 </html>
--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -1,8 +1,69 @@
 @extends('layouts.app')
+@section('title', 'Dashboard')
 @section('content')
-    <div class="hp-main-layout-content">
-        <div class="row mb-32 g-32">
-            
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="media">
+                        <div class="media-body overflow-hidden">
+                            <p class="text-truncate font-size-14 mb-2">Available Balance</p>
+                            <h4 class="mb-0">${{ number_format(0, 2) }}</h4>
+                        </div>
+                        <div class="text-primary">
+                            <i class="ri-stack-line font-size-24"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body border-top py-3">
+                    <div class="text-truncate">
+                        <span class="text-muted ml-2"><a href="#">See All Transactions</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="media">
+                        <div class="media-body overflow-hidden">
+                            <p class="text-truncate font-size-14 mb-2">Total Income</p>
+                            <h4 class="mb-0">${{ number_format(0, 2) }}</h4>
+                        </div>
+                        <div class="text-primary">
+                            <i class="ri-stack-line font-size-24"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body border-top py-3">
+                    <div class="text-truncate">
+                        <span class="text-muted ml-2"><a href="#">See All Transactions</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="media">
+                        <div class="media-body overflow-hidden">
+                            <p class="text-truncate font-size-14 mb-2">Total Withdarawls</p>
+                            <h4 class="mb-0">${{ number_format(0, 2) }}</h4>
+                        </div>
+                        <div class="text-primary">
+                            <i class="ri-stack-line font-size-24"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body border-top py-3">
+                    <div class="text-truncate">
+                        <span class="text-muted ml-2"><a href="#">See All Transactions</a></span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
