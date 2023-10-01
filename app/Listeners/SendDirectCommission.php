a41ff484b1bd04492c607a96243aca46fd18bc7e
Shakeel Ahmad <shakeel2717@gmail.com>
2023-10-01 23:56:44 +0500

    plan direct and indirect commision can be implment

--- app/Listeners/SendDirectCommission.php
+++ app/Listeners/SendDirectCommission.php
@@ -24,24 +24,32 @@ class SendDirectCommission
     {
         // getting this user
         $user = User::find($event->userPlan->user_id);
-        info($user);
-
         // checking if this user have valid direct referral
-        if ($user->refer == 'default') {
-            goto skipThisListener;
+        if ($user->refer != 'default') {
+            $upliner = User::where('username', $user->refer)->first();
+            $directCommission = $event->userPlan->amount * settings("direct_level_1_commission") / 100;
+
+            $transaction = $upliner->transactions()->create([
+                'type' => 'direct commission',
+                'amount' => $directCommission,
+                'sum' => true,
+                'status' => true,
+                'reference' => "direct commission form downline: " . $user->username,
+            ]);
+
+            // checking if this user have valid direct referral
+            if ($upliner->refer != 'default') {
+                $upliner1 = User::where('username', $upliner->refer)->first();
+                $indirectCommission = $event->userPlan->amount * settings("direct_level_2_commission") / 100;
+
+                $transaction = $upliner->transactions()->create([
+                    'type' => 'indirect commission',
+                    'amount' => $indirectCommission,
+                    'sum' => true,
+                    'status' => true,
+                    'reference' => "indirect commission form downline: " . $user->username,
+                ]);
+            }
         }
-
-        $upliner = User::where('username', $user->refer)->first();
-        $directCommission = $event->userPlan->amount * settings("direct_level_1_commission") / 100;
-
-        $transaction = $upliner->transactions()->create([
-            'type' => 'direct commission',
-            'amount' => $directCommission,
-            'sum' => true,
-            'status' => true,
-            'reference' => "direct commission form downline: " . $user->username,
-        ]);
-
-        skipThisListener:
     }
 }
