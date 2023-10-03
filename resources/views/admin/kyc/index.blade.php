0ad5cba6f5ac47c1ff7579b96e8202e74d0feb43
Shakeel Ahmad <shakeel2717@gmail.com>
2023-10-03 14:20:12 +0500

    KYC COmplete

--- resources/views/admin/kyc/index.blade.php
+++ resources/views/admin/kyc/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <div class="card">
+            <div class="">
                 <div class="card-body">
                     <h4 class="">@yield('title')</h4>
-                    <livewire:admin.all-kyc/>
+                    <livewire:admin.all-kyc-request/>
                 </div>
             </div>
         </div>
