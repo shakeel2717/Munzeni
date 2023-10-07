25afcbbcdd809dd7c7b823f4df32a4edd102542b
Shakeel Ahmad <shakeel2717@gmail.com>
2023-10-07 18:45:32 +0500

    data 50 show

--- app/Http/Livewire/TradeSection.php
+++ app/Http/Livewire/TradeSection.php
@@ -37,12 +37,12 @@ class TradeSection extends Component
 
     public function mount()
     {
-        $this->orderOneHistories = TradeHistory::where('type', 'one')->latest()->limit(5)->get();
+        $this->orderOneHistories = TradeHistory::where('type', 'one')->latest()->limit(50)->get();
 
-        $this->myOrderOneHistories = Trading::where('user_id', auth()->user()->id)->where('method', 'one')->latest()->limit(5)->get();
-        $this->myOrderFiveHistories = Trading::where('user_id', auth()->user()->id)->where('method', 'five')->latest()->limit(5)->get();
+        $this->myOrderOneHistories = Trading::where('user_id', auth()->user()->id)->where('method', 'one')->latest()->limit(50)->get();
+        $this->myOrderFiveHistories = Trading::where('user_id', auth()->user()->id)->where('method', 'five')->latest()->limit(50)->get();
 
-        $this->orderFiveHistories = TradeHistory::where('type', 'five')->latest()->limit(5)->get();
+        $this->orderFiveHistories = TradeHistory::where('type', 'five')->latest()->limit(50)->get();
 
         $this->bitcoinPrice =  0000000;
         $this->oneTimeSecond = date('s');
